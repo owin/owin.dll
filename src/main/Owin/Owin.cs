5aac97c1a905b01c7de68355c1f8fdb19fc0f5a8
Louis DeJardin <louis.dejardin@gmail.com>
2012-06-27 16:21:29 -0700

    Experimenting with use of Stream and Task primarily

--- src/main/Owin/Owin.cs
+++ src/main/Owin/Owin.cs
@@ -1,35 +1,43 @@
 ﻿﻿using System;
 using System.Collections.Generic;
+﻿using System.IO;
 ﻿using System.Threading;
 ﻿using System.Threading.Tasks;
 
 namespace Owin
 {
+    using AppAction = Action< // Call
+        IDictionary<string, object>, // Environment
+        IDictionary<string, string[]>, // Headers
+        Stream, // Body
+        CancellationToken, // CallCancelled
+        Task<Tuple< //Result
+            IDictionary<string, object>, // Properties
+            int, // Status
+            IDictionary<string, string[]>, // Headers
+            Action< // CopyTo
+                Stream, // Body
+                CancellationToken, // CopyToCancelled
+                Task>>>>; // Done
+
     public struct CallParameters
     {
         public IDictionary<string, object> Environment;
         public IDictionary<string, string[]> Headers;
-        public BodyDelegate Body;
-        public CancellationToken CallDisposed;
+        public Stream Body;
     }
 
     public struct ResultParameters
     {
+        public IDictionary<string, object> Properties;
         public int Status;
         public IDictionary<string, string[]> Headers;
         public BodyDelegate Body;
-        public IDictionary<string, object> Properties;
     }
 
-    public delegate void AppDelegate(CallParameters call, Action<ResultParameters, Exception> callback);
+    public delegate Task<ResultParameters> AppDelegate(CallParameters call, CancellationToken cancel);
 
-    public delegate Task<ResultParameters> AppTaskDelegate(CallParameters call);
-
-    public delegate void BodyDelegate(
-        Func<ArraySegment<byte>, Action<Exception>, bool> write,
-        Action<Exception> end,
-        CancellationToken cancel);
-
+    public delegate Task BodyDelegate(Stream output, CancellationToken cancel);
 
     public interface IAppBuilder
     {
